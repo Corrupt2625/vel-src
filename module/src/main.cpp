03ed9d352b192c615cdfa04e71504e43f16f8067
corrupt <98520172+Corrupt2625@users.noreply.github.com>
2025-05-26 22:45:37 +0200

    Update main.cpp

--- module/src/main.cpp
+++ module/src/main.cpp
@@ -1,4 +1,15 @@
 #include <Windows.h>
+#include <Richedit.h>
+#include <CommCtrl.h>
+#include <iostream>
+#include <fstream>
+#include <deque>
+#include <climits>
+#include <cstdio>
+#include <cstdlib>
+#include <string>
+#include <vector>
+#include <filesystem>
 
 BOOL WINAPI	DllMain(HINSTANCE handle, DWORD reason, LPVOID reserved) {
 	if (reason == 1) { // dll process attach
