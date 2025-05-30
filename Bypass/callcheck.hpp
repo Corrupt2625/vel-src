33645544a383dedbadcfd6b52b18459cb500c160
corrupt <98520172+Corrupt2625@users.noreply.github.com>
2025-05-30 11:15:51 +0200

    Update callcheck.hpp

--- Bypass/callcheck.hpp
+++ Bypass/callcheck.hpp
@@ -1,4 +1,16 @@
 #pragma once
 #import <iostream>
 
+class state(){
+  private:
+    uintptr_t rbxstate
+  public:
+    uintptr_t return_state(){
+      return rbxstate();
+    }
+    void set_state(uintptr_t rbx){
+      rbx=rbx;
+    }
+}
+
 void CALLCHECK_INSTANTIATED(state rbx);
