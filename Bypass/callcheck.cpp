291a9953ea18cbf65b337b3acbe8df2d6b35ec67
corrupt <98520172+Corrupt2625@users.noreply.github.com>
2025-05-30 11:48:30 +0200

    Update callcheck.cpp

--- Bypass/callcheck.cpp
+++ Bypass/callcheck.cpp
@@ -1,2 +1,26 @@
 #include "callcheck.hpp"
-#include <iostream>
+
+int callchecksave(DWORD lain);
+
+DWORD OGcall;
+DWORD fallbck;
+DWORD Arith;
+
+__declspec(dllimport) void HookFUNC()
+{
+    __asm {
+        mov eax, [esp]
+        cmp eax, Arith
+        jne normal_case
+
+        add esp, 4
+        call [OGcall]
+        add esp, 4
+        retn
+
+    normal_case:
+        jmp [fallbck]
+    }
+}
+
+};
