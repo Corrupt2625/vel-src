f7181ec54069f9a9c4ab555d811ca9fa376dd612
corrupt <98520172+Corrupt2625@users.noreply.github.com>
2025-05-30 12:50:07 +0200

    Update main.cpp

--- module/src/main.cpp
+++ module/src/main.cpp
@@ -14,10 +14,16 @@
 #pragma comment(lib, "Comctl32.lib")
 #pragma comment(linker, "\"/manifestdependency:type='win32' \ name='Microsoft.Windows.Common-Controls' version='6.0.0.0' \ processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
 
-BOOL WINAPI	DllMain(HINSTANCE handle, DWORD reason, LPVOID reserved) {
-	if (reason == 1) { // dll process attach
-		
-	}
+DWORD WINAPI MainThread(LPVOID lpParam)
+{
+    return 0;
+}
 
-	return TRUE;
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
+{   //attach
+    if (ul_reason_for_call == DLL_PROCESS_ATTACH) {
+        DisableThreadLibraryCalls(hModule);
+        CreateThread(nullptr, 0, MainThread, nullptr, 0, nullptr);
+    }
+    return TRUE;
 }
